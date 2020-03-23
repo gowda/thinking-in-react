4d354901eca903a9ea898ceb0d1e4e0fd5901300
Basavanagowda Kanur <basavanagowda@gmail.com>
2020-03-23 15:53:48 +0100

    Implement static product listing site
    From:
    https://reactjs.org/docs/thinking-in-react.html#step-2-build-a-static-version-in-react

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,136 @@
 import React, { Component } from 'react';
 import './app.css';
 
+class ProductCategoryRow extends Component {
+  render() {
+    return (
+      <tr>
+        <th colSpan="2">{this.props.category}</th>
+      </tr>
+    );
+  }
+}
+
+class ProductRow extends Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked
+      ? product.name
+      : (
+        <span style={{ color: 'red' }}>
+          {product.name}
+        </span>
+      );
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends Component {
+  render() {
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow category={product.category} key={product.category} />,
+        );
+      }
+      rows.push(
+        <ProductRow product={product} key={product.name} />,
+      );
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+}
+
+const PRODUCTS = [
+  {
+    category: 'Sporting Goods',
+    price: '$49.99',
+    stocked: true,
+    name: 'Football',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$9.99',
+    stocked: true,
+    name: 'Baseball',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$29.99',
+    stocked: false,
+    name: 'Basketball',
+  },
+  {
+    category: 'Electronics',
+    price: '$99.99',
+    stocked: true,
+    name: 'iPod Touch',
+  },
+  {
+    category: 'Electronics',
+    price: '$399.99',
+    stocked: false,
+    name: 'iPhone 5',
+  },
+  {
+    category: 'Electronics',
+    price: '$199.99',
+    stocked: true,
+    name: 'Nexus 7',
+  },
+];
+
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <h1>Hello, world!</h1>
-      </div>
+      <FilterableProductTable products={PRODUCTS} />
     );
   }
 }
