9eff8ecfc9c51982c6e302f4502c8ec3d8ffdf62
Basavanagowda Kanur <basavanagowda@gmail.com>
2020-03-23 14:54:48 +0100

    Add & configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'max-classes-per-file': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+  },
+};
--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,9 @@ import './app.css';
 class App extends Component {
   render() {
     return (
-    <div className='app'>
-      <h1>Hello, world!</h1>
-    </div>
+      <div className="app">
+        <h1>Hello, world!</h1>
+      </div>
     );
   }
 }
