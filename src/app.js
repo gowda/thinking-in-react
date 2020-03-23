e7385011f82a77a036f8b272f524d99c713d4332
Basavanagowda Kanur <basavanagowda@gmail.com>
2020-03-23 14:32:55 +0100

    Create minimal skeleton for a react app without hot module reloading
    Details on:
    https://blog.usejournal.com/creating-a-react-app-from-scratch-f3c693b84658

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className='app'>
+      <h1>Hello, world!</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
